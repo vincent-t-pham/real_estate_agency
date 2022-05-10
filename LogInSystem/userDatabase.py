0fcc38e7764b6ece5dc96b3a604bcfabb415e108
Wayne Flores <waynef711@gmail.com>
2022-05-09 20:39:00 -0700

    adjust userDatabase.py to include existing users from table.py

--- LogInSystem/userDatabase.py
+++ LogInSystem/userDatabase.py
@@ -36,6 +36,25 @@ password = encryptPassword("admin123")
 c.execute("INSERT INTO users VALUES('admin1', ?)", (password,))
 c.execute("INSERT INTO admins VALUES('admin1')")
 
+# add the agents
+password = encryptPassword("password")
+c.execute("INSERT INTO users VALUES('0985820', ?)", (password,)) # Anthony Edward
+c.execute("INSERT INTO users VALUES('0975482', ?)", (password,)) # Paul Rudd
+c.execute("INSERT INTO users VALUES('0989589', ?)", (password,)) # Anakin Skywalker
+c.execute("INSERT INTO users VALUES('0986289', ?)", (password,)) # Jesse Pinkman
+
+# add the clients
+c.execute("INSERT INTO users VALUES('anikha69', ?)", (password,)) # Anikha S
+c.execute("INSERT INTO users VALUES('christineio', ?)", (password,)) # Christine Polly
+c.execute("INSERT INTO users VALUES('stephenstrange', ?)", (password,)) # Stephen S
+c.execute("INSERT INTO users VALUES('tony_s', ?)", (password,)) # Tony S
+
+# add the owners
+c.execute("INSERT INTO users VALUES('jimmy23', ?)", (password,)) # Jimmy W
+c.execute("INSERT INTO users VALUES('walter07', ?)", (password,)) # Walter S
+c.execute("INSERT INTO users VALUES('aaron98', ?)", (password,)) # Aaron P
+c.execute("INSERT INTO users VALUES('hina87', ?)", (password,)) # Hina S
+
 # Save changes
 conn.commit()
 
